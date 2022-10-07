bb21d9daa7d0f6b7f1899cb856042abb30dbc751
Emanuel <santindosanjos@gmail.com>
2022-10-07 20:08:01 -0300

    ultimas alterações

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8pq-yCsKxbpHghN3EYBaUW__UglvCF8c",
+  authDomain: "apputfprnews.firebaseapp.com",
+  projectId: "apputfprnews",
+  storageBucket: "apputfprnews.appspot.com",
+  messagingSenderId: "1080449491976",
+  appId: "1:1080449491976:web:9a7c3f3d2eacd558969dbd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
